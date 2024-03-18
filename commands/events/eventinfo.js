af1996dfde219d6ec5bb32847785cc0415370bf9
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2024-03-18 21:43:01 +1100

    Add help command

--- commands/events/eventinfo.js
+++ commands/events/eventinfo.js
@@ -21,50 +21,44 @@ module.exports = {
 
         try {
             // Make the API request
-			const response = await request(apiUrl);
-			const responseBody = await response.body.json();
+            const response = await request(apiUrl);
+            const responseBody = await response.body.json();
 
-            let { title, url, ctftime_url, start, finish, description, organizers } = responseBody;
-
-            organizers = organizers[0]['name'];
+            const { title, url, ctftime_url, start, finish, description, organizers } = responseBody;
 
             const descriptionMaxChars = 64000;
-            if (description.length > descriptionMaxChars) {
-                description = description.substring(0, descriptionMaxChars) + '...';
-            }
+            const truncatedDescription = description.length > descriptionMaxChars ? description.substring(0, descriptionMaxChars) + '...' : description;
 
             const startDate = convertDateUTCToAEDT(new Date(start));
             const finishDate = convertDateUTCToAEDT(new Date(finish));
             const startReadable = convertDateReadable(startDate, true);
             const finishReadable = convertDateReadable(finishDate, true);
 
-            const { days, hours } = timeBetween(today, convertDateUTCToAEDT(startDate))
+            const { days, hours } = timeBetween(today, convertDateUTCToAEDT(startDate));
 
-			const responseEmbed = new EmbedBuilder()
-				.setColor(0xe3000b)
-				.setTitle(`CTFTime: ${title}`)
+            const responseEmbed = new EmbedBuilder()
+                .setColor(0xe3000b)
+                .setTitle(`CTFTime: ${title}`)
                 .setURL(url)
                 .setAuthor({ name: 'CTFTime', iconURL: 'https://play-lh.googleusercontent.com/uiZnC5tIBpejW942OXct4smbaHmSowdT5tLSi28Oeb2_pMLPCL-VJqdGIH6ZO3A951M', url: 'https://ctftime.org' })
-                .setDescription(description)
-                .setFooter({ text: 'See this week\s events with /upcoming'})
-                
-            responseEmbed.addFields({ 
-                name: `Time`,
-                value: `
-                ${startReadable} - ${finishReadable}
-                (in ${days} days, ${hours} hours)`
-            });
-            responseEmbed.addFields({ 
-                name: `Event Site`,
-                value: `[Event Link](${url})`,
-                inline: true
-            });
-            responseEmbed.addFields({ 
-                name: `Event Site`,
-                value: `[CTFTime Page](${ctftime_url})`,
-                inline: true
-            });
-
+                .setDescription(truncatedDescription)
+                .setFooter({ text: 'See this week\s events with /upcoming' })
+                .addFields(
+                    { 
+                        name: `Time`,
+                        value: `${startReadable} - ${finishReadable}\n(in ${days} days, ${hours} hours)`
+                    },
+                    { 
+                        name: `Event Site`,
+                        value: `[Event Link](${url})`,
+                        inline: true
+                    },
+                    { 
+                        name: `CTFTime Page`,
+                        value: `[CTFTime Page](${ctftime_url})`,
+                        inline: true
+                    }
+                );
 
 			// Process the response and send it to the Discord bot
 			await interaction.editReply({ embeds: [responseEmbed] });
